bba1bb9743114ff827630759e63cedafa29737b6
joshua.prichard <joshuawprichard@gmail.com>
2023-06-20 14:08:56 +1200

    New: - sub-tile component for displaying subscriptions in a tile format.
    Modified:
    - Add css to app.component.scss to apply styles to content in app.
    - Add css to styles.scss so that teh background colour of the app can be set.
    
    Removed:
    - Removed HTML from app.component.html as it was boilerplate angular code.

---
+++ SubManagerApp/src/app/sub-tile/sub-tile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-tile',
+  templateUrl: './sub-tile.component.html',
+  styleUrls: ['./sub-tile.component.scss']
+})
+export class SubTileComponent {
+  title="Netflix"
+  price="$23"
+  frequency="Monthly"
+}

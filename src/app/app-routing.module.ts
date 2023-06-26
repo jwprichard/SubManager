04896d05b39935cb1c1dd1963b4bad31004e816e
joshua.prichard <joshuawprichard@gmail.com>
2023-06-27 11:48:21 +1200

    Add routing to app through navbar.component.html
    Add marketplace.component.html as template home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SubTileControllerComponent } from './Components/sub-tile-controller/sub-tile-controller.component';
+import { MarketplaceComponent } from './Components/marketplace/marketplace.component';
 
 const routes: Routes = [
-  {path: 'subscriptions', component: SubTileControllerComponent}
+  {path: 'subscriptions', component: SubTileControllerComponent},
+  {path: 'marketplace', component: MarketplaceComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SubTileComponent } from './Components/sub-tile/sub-tile.component';
 import { SubTileControllerComponent } from './Components/sub-tile-controller/sub-tile-controller.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { MarketplaceComponent } from './Components/marketplace/marketplace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SubTileComponent,
     SubTileControllerComponent,
-    NavbarComponent
+    NavbarComponent,
+    MarketplaceComponent
   ],
   imports: [
     BrowserModule,
